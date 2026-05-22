e87983441bb3d204e1fa89f9fbb97a9c58fc5769
btmteknikmarketing <alperenturkoglu088@gmail.com>
2026-05-22 09:52:49 +0300

    test

--- awp_site/urls.py
+++ awp_site/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 #test
-#test
+
 #test
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -15,8 +15,7 @@ if settings.DEBUG:
 
 
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#test
-#test#test#test
+
 #test
 #test
 #test
